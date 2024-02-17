fad180eaee0bfb8b9341e0eaec088247da482405
Giovanni Witulski <witulski.schule@gmail.com>
2024-02-17 18:11:34 +0100

    added logic for right/wrong answers

--- script.js
+++ script.js
@@ -1,62 +1,75 @@
 let questions = [
     {
-         "question": "frage",
-         "answer1": "antwort",
-         "answer2": "antwort",
-         "answer3": "antwort",
-         "answer4": "antwort",
-         "right-answer": 1
+         "question": "Was ist die Hauptstadt von Frankreich?",
+         "answer1": "a) Berlin",
+         "answer2": "b) Madrid",
+         "answer3": "c) Rom",
+         "answer4": "d) Paris",
+         "right-answer": 4
     },
     {
-        "question": "frage",
-        "answer1": "antwort",
-        "answer2": "antwort",
-        "answer3": "antwort",
-        "answer4": "antwort",
-        "right-answer": 1
+        "question": "Welches Element hat das chemische Symbol <b>H</b> ?",
+        "answer1": "a) Helium",
+        "answer2": "b) Wasserstoff",
+        "answer3": "c) Stickstoff",
+        "answer4": "d) Sauerstoff",
+        "right-answer": 2
    },
    {
-        "question": "frage",
-        "answer1": "antwort",
-        "answer2": "antwort",
-        "answer3": "antwort",
-        "answer4": "antwort",
+        "question": "Wer hat das Gemälde <b>Die Mona Lisa</b> gemalt?",
+        "answer1": "a) Vincent van Gogh",
+        "answer2": "b) Leonardo da Vinc",
+        "answer3": "c) Pablo Picasso",
+        "answer4": "d) Michelangelo",
+        "right-answer": 2
+    },
+    {
+        "question": "Welcher Planet ist der vierte in unserem Sonnensystem?",
+        "answer1": "a) Mars",
+        "answer2": "b) Jupiter",
+        "answer3": "c) Venus",
+        "answer4": "d) Saturn",
         "right-answer": 1
     },
     {
-        "question": "frage",
-        "answer1": "antwort",
-        "answer2": "antwort",
-        "answer3": "antwort",
-        "answer4": "antwort",
-        "right-answer": 1
-    },
-    {
-        "question": "frage",
-        "answer1": "antwort",
-        "answer2": "antwort",
-        "answer3": "antwort",
-        "answer4": "antwort",
-        "right-answer": 1
+        "question": "Welcher berühmte Physiker formulierte die Relativitätstheorie?",
+        "answer1": "a) Isaac Newton",
+        "answer2": "b) Albert Einstein",
+        "answer3": "c) Stephen Hawking",
+        "answer4": "d) Galileo Galilei",
+        "right-answer": 2
     },
 ];
+
+let rightQuestions = 0;
 
 let currentQuestion = 0;
 
 function init() {
     document.getElementById('all-questions').innerHTML = questions.length;
-
     showQuestion();
 }
 
 function showQuestion() {
-    let question = questions[currentQuestion];
 
-    document.getElementById('questiontext').innerHTML = question['question'];
-    document.getElementById('answer1').innerHTML = question['answer1'];
-    document.getElementById('answer2').innerHTML = question['answer2'];
-    document.getElementById('answer3').innerHTML = question['answer3'];
-    document.getElementById('answer4').innerHTML = question['answer4'];
+    if (currentQuestion >= questions.length) {
+        document.getElementById('endScreen').style = '';
+        document.getElementById('questionBody').style = 'display: none;';
+
+        document.getElementById('amountOfQuestions').innerHTML = questions.length;
+        document.getElementById('amountOfRightQuestions').innerHTML = rightQuestions;
+
+        document.getElementById('headerImg').src = 'img/trophy.png';
+    } else {
+        let question = questions[currentQuestion];
+
+        document.getElementById('question-number').innerHTML = currentQuestion + 1;
+        document.getElementById('questiontext').innerHTML = question['question'];
+        document.getElementById('answer1').innerHTML = question['answer1'];
+        document.getElementById('answer2').innerHTML = question['answer2'];
+        document.getElementById('answer3').innerHTML = question['answer3'];
+        document.getElementById('answer4').innerHTML = question['answer4'];        
+    }
 }
 
 function answer(selection) {
@@ -65,13 +78,30 @@ function answer(selection) {
     let idOfRightAnswer = `answer${question['right-answer']}`;
     
     if (selectetAnswer == question['right-answer']) {
-        console.log('richtige Antwort');
         document.getElementById(selection).parentNode.classList.add('bg-success');
-
+        rightQuestions++;
     } else {
         console.log('falsche Antwort');
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
     }
     document.getElementById('nextBtn').disabled = false;
+}
+
+function nextQuestion() {
+    currentQuestion++;
+    document.getElementById('nextBtn').disabled = true;
+    resetAnswerBtns();
+    showQuestion();
+}
+
+function resetAnswerBtns() {
+    document.getElementById('answer1').parentNode.classList.remove('bg-danger')
+    document.getElementById('answer1').parentNode.classList.remove('bg-success')
+    document.getElementById('answer2').parentNode.classList.remove('bg-danger')
+    document.getElementById('answer2').parentNode.classList.remove('bg-success')
+    document.getElementById('answer3').parentNode.classList.remove('bg-danger')
+    document.getElementById('answer3').parentNode.classList.remove('bg-success')
+    document.getElementById('answer4').parentNode.classList.remove('bg-danger')
+    document.getElementById('answer4').parentNode.classList.remove('bg-success')
 }
